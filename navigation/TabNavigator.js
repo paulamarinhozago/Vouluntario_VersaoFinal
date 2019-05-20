1cd1eee1743f4316baa9c374e8fd448fd6d2d276
pmzago <t-pamari@microsoft.com>
2019-05-20 00:38:15 -0300

    commit all

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -3,10 +3,19 @@ import Search from '../screens/Search'
 import Upload from '../screens/Upload'
 import Activity from '../screens/Activity'
 import Profile from '../screens/Profile'
+import { Ionicons } from '@expo-vector-icons'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({
-  Home: Home,
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: '',
+      tabBarIcon: () => {
+          <Ionicons name = 'ios-home' size={32} />
+      }
+    }
+  },
   Search: Search,
   Upload: Upload,
   Activity: Activity,

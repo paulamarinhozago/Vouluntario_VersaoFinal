3423fcf3dfd53fb716996000a2fbac5c0c6c8bb2
pmzago <t-pamari@microsoft.com>
2019-05-19 23:36:23 -0300

    commit all

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import styles from '../'
 
 export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Home</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

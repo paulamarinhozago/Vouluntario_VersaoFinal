3b795cb85ae2493884d8b27f117b0db0de99a3e4
pmzago <t-pamari@microsoft.com>
2019-05-20 15:21:58 -0300

    commit all

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { connect } from 'react-redux'
 import styles from '../styles'
 
-class Profile extends React.Component {
+export default class Profile extends React.Component {
   render() {
     return (
       <View style={styles.container}>

226ba68ad55fab881105f1ff14ec991e2aae82bf
pmzago <t-pamari@microsoft.com>
2019-05-19 23:49:53 -0300

    commit all

--- screens/Search.js
+++ screens/Search.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import styles from '../styles'
 
-export default class Activity extends React.Component {
+export default class Search extends React.Component {
   render() {
     return (
       <View style={styles.container}>

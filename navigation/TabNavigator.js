3c5fc63b1fe7ca4abfcf5a9ccddce304539c9256
pmzago <t-pamari@microsoft.com>
2019-05-20 02:31:40 -0300

    commit all

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -13,7 +13,7 @@ const TabNavigator = createBottomTabNavigator({
     navigationOptions: {
       tabBarLabel: ' ',
       tabBarIcon: () => {
-          <Ionicons name = 'md-home' size={32} />
+          <Ionicons name = {'ios-home'} size={32} />
       }
     }
   },

212aff0c4ab2a8d9621e02242f74046a856f60fe
pmzago <t-pamari@microsoft.com>
2019-05-20 00:17:46 -0300

    commit all

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,10 +1,16 @@
 import Home from '../screens/Home'
 import Search from '../screens/Search'
+import Upload from '../screens/Upload'
+import Activity from '../screens/Activity'
+import Profile from '../screens/Profile'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({
   Home: Home,
   Search: Search,
+  Upload: Upload,
+  Activity: Activity,
+  Profile: Profile
 });
 
 export default createAppContainer(TabNavigator);

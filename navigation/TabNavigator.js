fe8b99ede6b57f67ce574ec0618406d7aadce834
pmzago <t-pamari@microsoft.com>
2019-05-20 01:00:10 -0300

    commit all

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,9 +1,10 @@
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import Home from '../screens/Home'
 import Search from '../screens/Search'
 import Upload from '../screens/Upload'
 import Activity from '../screens/Activity'
 import Profile from '../screens/Profile'
-import Ionicons from 'expo/vector-icons';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({

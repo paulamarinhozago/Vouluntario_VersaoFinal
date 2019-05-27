be7293cbaaf2069a447b9376a19ce182ef82a57a
pmzago <t-pamari@microsoft.com>
2019-05-27 15:29:04 -0300

    commit all

--- actions/user.js
+++ actions/user.js
@@ -18,7 +18,7 @@ export const updateType = (type) => {
     return {type: 'UPDATE_TYPE', payload: type}
 }
 
-export const login = (email, password) => {
+export const login = () => {
     return async (dispatch, getState) => {
         try {
             const { email, password } = getState().user
@@ -34,7 +34,7 @@ export const getUser = (uid) => {
     return async (dispatch, getState) => {
         try {
             const user = await db.collection('users').doc(uid).get()
-            dispatch({type: 'LOGIN', payload: user.data()})
+            dispatch({type: 'LOGIN', payload: user})
         } catch (e) {
             alert(e)
         }

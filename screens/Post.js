6b16f0d9b9891aee5aa48a0ab3e7c60f3eb2cd5d
pmzago <t-pamari@microsoft.com>
2019-06-03 13:25:06 -0300

    commit all

--- screens/Post.js
+++ screens/Post.js
@@ -4,12 +4,13 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Location, Permissions } from 'expo';
 import { updateDescription, updateLocation, uploadPost } from '../actions/post'
-import { Modal, SafeAreaView, Text, View, TextInput, TouchableOpacity, Keyboard, Image } from 'react-native';
-
+import { FlatList, Modal, SafeAreaView, Text, View, TextInput, TouchableOpacity, Keyboard, Image } from 'react-native';
+const GOOGLE_API = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 
 class Post extends React.Component {
   state = {
-    showModal: false
+    showModal: false, 
+    locations: []
   }
 
   post = () => {
@@ -18,27 +19,49 @@ class Post extends React.Component {
   }
 
   setLocation = (location) => {
+    const place = {
+      name: location.name,
+      coords: {
+        lat: location.geometry.location.lat,
+        lng: location.geometry.location.lng
+      }
+    }
     this.setState({ showModal: false })
-    this.props.updateLocation(location)
+    this.props.updateLocation(place)
   }
 
-  modal = () => {
-    return (
-      <Modal animationType='slide' transparent={false} visible={this.state.showModal}>
-        <SafeAreaView style={[styles.container, styles.center]}>
-          <TouchableOpacity style={styles.border} onPress={() => this.setLocation('Philadelphia')}>
-            <Text style={styles.gray}>Philadelphia</Text>
-          </TouchableOpacity>
-        </SafeAreaView>
-      </Modal>
-    )
+  getLocations = async () => {
+    this.setState({ showModal: true })
+    const permission = await Permissions.askAsync(Permissions.LOCATION)
+    if (permission.status === 'granted') {
+      console.log(permission)
+      const location = await Location.getCurrentPositionAsync()
+      console.log(location)
+      const url = `${GOOGLE_API}?location=${location.coords.latitude},${location.coords.longitude}&rankby=distance&key=${AIzaSyCyrrMwcw9_XPoHx8m7FnMJ2NkGjBR8Zog}`
+      const response = await fetch(url)
+      const data = await response.json()
+      this.setState({ locations: data.results })
+      console.log( data )
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        {this.modal()}
-        <Image style={styles.postPhoto} source={{uri: this.props.post.photo}}/>
+        <Modal animationType='slide' transparent={false} visible={this.state.showModal}>
+          <SafeAreaView style={[styles.container, styles.center]}>
+            <FlatList
+              keyExtractor={(item) => item.id}
+              data={this.state.locations}
+              renderItem={({ item }) => (
+              <TouchableOpacity style={styles.border} onPress={() => this.setLocation(item)}>
+                <Text style={styles.gray}>{item.name}</Text>
+                <Text style={styles.gray}>{item.vicinity}</Text>
+              </TouchableOpacity>
+            )}/> 
+          </SafeAreaView>
+        </Modal>
+      	<Image style={styles.postPhoto} source={{uri: this.props.post.photo }}/>
         <TextInput
             style={styles.inputBox2}
             multiline = {true}
@@ -49,10 +72,10 @@ class Post extends React.Component {
             value={this.props.post.postDescription}
             placeholder = 'Descrição'
         />
-        <TouchableOpacity style={styles.border} onPress={() => this.setState({showModal: true})}>
-          <Text style={styles.gray}>{this.props.post.location ? this.props.post.location : 'Add a Location'}</Text>
+        <TouchableOpacity style={styles.border} onPress={this.getLocations}>
+          <Text style={styles.gray}>{this.props.post.location ? this.props.post.location.name : 'Add a Location'}</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button_1} onPress={this.post} >
+      	<TouchableOpacity style={styles.button_1} onPress={this.post} >
             <Text style={{color: 'white', fontSize:18, fontWeight:'bold'}}>Compartilhar</Text>
         </TouchableOpacity>
       </View>

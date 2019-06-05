c4376c2840df25d5c96e52b021630d4d74d0059d
pmzago <t-pamari@microsoft.com>
2019-06-05 15:16:29 -0300

    commit all

--- actions/index.js
+++ actions/index.js
@@ -5,7 +5,7 @@ import { ImageManipulator } from 'expo';
 export const uploadPhoto = (image) => {
   return async (dispatch) => {
     try {
-      const resize = await ImageManipulator.manipulateAsync(image.uri, [], { format: 'jpg', compress: 0.2 })
+      const resize = await ImageManipulator.manipulateAsync(image.uri, [], { format: 'jpg', compress: 0.1 })
       const blob = await new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
         xhr.onload = () => resolve(xhr.response)

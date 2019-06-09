e13447286a50bcdd6915933e2b360459c9379762
pmzago <t-pamari@microsoft.com>
2019-06-09 19:58:26 -0300

    commit all

--- screens/Comment.js
+++ screens/Comment.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from '../styles'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Text, View, TextInput, FlatList, Image, KeyboardAvoidingView } from 'react-native';
-import { addComment, getComments } from '../actions/post';
+import { Text, View, TextInput } from 'react-native';
+import { addComment } from '../actions/post';
 
 class Comment extends React.Component {
   state = {
@@ -23,39 +23,27 @@ class Comment extends React.Component {
 
   render() {
     return (
-      <KeyboardAvoidingView enabled behavior='padding' style={styles.container2}>
-        <FlatList
-          keyExtractor={(item) => JSON.stringify(item.date)}
-          data={this.props.post.comments}
-          renderItem={({item}) => (
-            <View style={[styles.row, styles.space]}>
-              <Image style={styles.roundImage} source={{uri: item.commenterPhoto}}/>
-              <View style={[styles.container, styles.left]}>
-                <Text>{item.commenterName}</Text>
-                <Text>{item.comment}</Text>
-              </View>
-            </View>
-          )}/>
-  	      <TextInput
-  	        style={styles.input}
-  	        onChangeText={(comment) => this.setState({comment})}
-  	        value={this.state.comment}
-  	        returnKeyType='send'
-            placeholder='Escreva um comentário!'
-            onSubmitEditing={this.postComment}/>
-      </KeyboardAvoidingView>
+      <View style={styles.container}>
+        <Text>{this.state.comment}</Text>
+	      <TextInput
+	        style={styles.input}
+	        onChangeText={(comment) => this.setState({comment})}
+	        value={this.state.comment}
+	        returnKeyType='send'
+          placeholder='Add Comment'
+          onSubmitEditing={this.postComment}/>
+      </View>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ addComment, getComments }, dispatch)
+  return bindActionCreators({ addComment }, dispatch)
 }
 
 const mapStateToProps = (state) => {
   return {
-    user: state.user,
-    post: state.post
+    user: state.user
   }
 }
 

c9b7243e76b929a7b31ed149bfb6b0c2e7eb55c9
pmzago <t-pamari@microsoft.com>
2019-06-16 17:56:10 -0300

    commit all

--- screens/Home.js
+++ screens/Home.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { getPosts, likePost, unlikePost } from '../actions/post'
 import styles from '../styles'
+import moment from 'moment'
 
 class Home extends React.Component {
 
@@ -22,9 +23,9 @@ class Home extends React.Component {
   }
 
   navigateMap = (item) => {
-    this.props.navigation.navigate('Map', 
-      { location: item.postLocation }
-    )
+    this.props.navigation.navigate('Map', { 
+      location: item.postLocation 
+    })
   }
 
   render() {
@@ -32,33 +33,40 @@ class Home extends React.Component {
     return (
       <View style={styles.container}>
         <FlatList
+          onRefresh={() => this.props.getPosts()}
+          refreshing={false}
           data={this.props.post.feed}
           keyExtractor={(item) => item.id}
-          renderItem={({item}) => (
-            <View>
-              <View style={[styles.row, styles.center]}>
-                <View style={[styles.row, styles.center]}>
-                  <Image style={styles.roundImage} source={{uri: item.photo}}/>
-                  <View>
-                    <Text>{item.username}</Text>
-                    <TouchableOpacity onPress={() => this.navigateMap(item)} >
-                      <Text>{item.postLocation ? item.postLocation.name : null}</Text>
-                    </TouchableOpacity>
-                  </View>
-                  </View>
-                    <Ionicons style={{margin: 5}} name='md-calendar' size={25} />
-                  </View>
-                  <TouchableOpacity onPress={() => this.likePost(item)} >
-                    <Image style={styles.postPhoto} source={{uri: item.postPhoto}}/>
-                  </TouchableOpacity>
-                  <View style={styles.row}><Ionicons name='md-person-add' style={{margin: 5}} color={item.likes.includes(this.props.user.uid) ? 'springgreen' : 'black'} size={25} />
-                      <TouchableOpacity onPress={() => this.props.navigation.navigate('Comment', item)} >
-                        <Ionicons style={{margin: 5}} name='ios-chatbubbles' size={25} />
+          renderItem={({item}) => {
+            const liked = item.likes.includes(this.props.user.uid)
+            return (
+              <View>
+                <View style={[styles.row, styles.space]}>
+                  <View style={[styles.row, styles.center]}>
+                    <Image style={styles.roundImage} source={{uri: item.photo}}/>
+                    <View>
+                      <Text style={styles.bold}>{item.username}</Text>
+                      <Text style={[styles.gray, styles.small]}>{moment(item.date).format('ll')}</Text>
+                      <TouchableOpacity onPress={() => this.navigateMap(item)} >
+                        <Text>{item.postLocation ? item.postLocation.name : null}</Text>
                       </TouchableOpacity>
+                    </View>
                   </View>
-                  <Text style={{ width: 300 }}>{item.postDescription}</Text>
+                  <Ionicons style={{margin: 5}} name='md-calendar' size={25} />
                 </View>
-          )}
+                <TouchableOpacity onPress={() => this.likePost(item)} >
+                  <Image style={styles.postPhoto} source={{uri: item.postPhoto}}/>
+                </TouchableOpacity>
+                <View style={styles.row}>
+                  <Ionicons name='md-person-add' style={{margin: 5}} color={item.likes.includes(this.props.user.uid) ? 'springgreen' : 'black'} size={25} />
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Comment', item)} >
+                      <Ionicons style={{margin: 5}} name='ios-chatbubbles' size={25} />
+                  </TouchableOpacity>
+                </View>
+                <Text style={{ width: 300 }}>{item.postDescription}</Text>
+              </View>
+            )
+          }}
         />
       </View>
     );
@@ -77,5 +85,7 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+
 
 
